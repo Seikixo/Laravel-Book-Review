3601318f00c24000f21a2fdc190f2ca7f1217a26
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-10-21 14:46:28 +0800

    Relationship between Book and Review

---
+++ book-review/app/Models/Review.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    public function books(){
+        return $this->belongsTo(Book::class);
+    }
+}
